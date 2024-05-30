ca7aab643bead3accfcaac0c754c5d72c3af8a47
Your Name <you@example.com>
2024-05-30 14:41:59 +0800

    data_type

---
+++ data_type.py
@@ -0,0 +1,13 @@
+# multiple lines 
+multipleline = '''
+ABC
+DEF
+        GHIJK
+'''
+print(multipleline)
+
+
+# Escaping special char
+
+sentence = 'I\'m back at work!\tHey!\n\nWhere\'s this at\\located?'
+print(sentence)
